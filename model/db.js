e2ec0d720ebfa19f89a1ad70ce45efc80dc69594
shikhadubey <get2shikhadubey@gmail.com>
2016-04-01 16:58:35 +0530

    db.js
    Creating connection to db.

---
+++ model/db.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var util = require('util');
+var mysequel = require('mysequel');
+
+var config = {
+    user: 'root',
+    password: 'mysql',
+    host: 'localhost',
+    database: 'mktplace'
+}
+
+var dbConfig = {
+ url: util.format('mysql://%s:%s@%s:3306/%s',
+   encodeURIComponent(config.user),
+   encodeURIComponent(config.password),
+   config.host,
+   config.database),
+ connections: {
+   min: config.min || 2,
+   max: config.max || 10
+ }
+};
+
+var db = mysequel(dbConfig);
+
+
+module.exports = db;
