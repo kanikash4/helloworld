28945555929880c0f6b52f4f54f89d726969d3e2
kanika sharma <kanikash4@gmail.com>
2016-04-21 18:09:00 +0530

    creation and fetching of hash

--- model/hash/index.js
+++ model/hash/index.js
@@ -1,69 +1,59 @@
 'use strict';
 
 var db = require('../db');
-
+var bcrypt = require('bcrypt');
 var hm = {
-  table:db.define({
-    name: 'userHash',
-    columns:['email','hash', 'created_at', 'updated_at']
-  }),
+            table       :db.define({
+            name        : 'userHash',
+            columns     :['email','hash', 'created_at', 'updated_at']
+          }),
   create: function createfn(userId, cb) {
     // create a new hash for this user and save it hash table
-    //   if (!data.email ) {
-    //   return cb(new Error('Email not retrieved'));
-    // }
-    // hm.isEmailAvailable(data.email, function (err, exists) {
-    //   if (err || exists) {
-    //     return cb(err || new Error('User already exists.. Cannot Add: ' + data.email));
-    //   } else {
-    //     encrypt(data.password, function (err, hash) {
-    //       if (err || !hash) {
-    //         return cb(err || new Error('Unable to generate password hash'));
-    //       }
-    //       //add userhash
-    //       var userentry = {
-    //         email       : data.email,
-    //         hash        : hash,
-    //         created_at  : new Date()
-    //       };
-
-    //       var query = hm.table.insert(userentry);
-
-    //       query.exec(function (err, res) {
-    //         console.log(err || res);
-
-    //         if (res) {
-    //           userentry.id = res.insertId;
-    //         }
-
-    //         cb(err, userentry);
-    //       });
-    //     });
-    //   }
+        //generate a salt
+        var salt = bcrypt.genSaltSync(10);
+        //hash the password with salt
+        var hash = bcrypt.hashSync("mypassword", salt);
+         //storing the hash in db
+         var data = 'kanikash4@gmail.com';
+         //add userhash
+          var userentry = {
+            email       : data.email,
+            hash        : hash,
+            created_at  : new Date()
+          };
+         var query = hm.table.insert(userentry);
+         query.exec(function(err, res){
+          console.log(err||res);
+          if(res){
+            console.log("done");
+          }
+          cb(err, userentry)
+         })
     
     // // invlidate older hashes if exist for this user;
   },
   
   fetch: function fetch(keys, options, cb) {
-    // fetch hashes with given keys as filters
-    // var tbl = hm.table;
-    // var query = tbl.select(options.selectFields);
-    // var filters = [];
-    // Object.keys(keys).forEach(function (key) {
-    //   if (keys[key]) {
-    //     filters.push(tbl[key].equals(keys[key]));
-    //   }
-    // });
-    // if (filters.length) {
-    //   query = query.where.apply(tbl, filters);
-    // }
-    // console.log(query.toQuery());
+    //fetch hashes with given keys as filters
+    hm.table.select(options.selectFields);
+    var filters = [];
+    Object.keys(keys).forEach(function (key) {
+      if (keys[key]) {
+        filters.push(tbl[key].equals(keys[key]));
+      }
+    });
+    if (filters.length) {
+      query = query.where.apply(tbl, filters);
+    }
+    console.log(query.toQuery());
 
-    // query.exec(cb);
+    query.exec(cb);
   },
   
   isValidHash: function validatefn(hash, cb) {
     // check if this hash exists and has not expired
+    bcrypt.compareSync("my password",hash);//true
+    bcrypt.compareSync("not my password",hash);//false
   },
   
   resetHash: function resetHashfn(userId, hash, cb) {
@@ -75,32 +65,23 @@ module.exports = hm;
 
 ///self test
 
-
 if (require.main === module) {
   (function () {
-
-
-    var data = {
-      // email: 'kanikash4@gmail.com',
-      //   password: 'test'
-
-    };
+    // var data = {
+    //    email: 'kanikash4@gmail.com'
+    // };
 
     //to run create function
-    // hm.create(data, function (err, res) {
-    //   console.log(err || res);
-    // });
-
-
+    hm.create(data, function (err, res) {
+      console.log(err || res);
+    });
     var keys = {
       id: 1
     };
-
     var options = {
-      selectFields: ['id', 'email', 'firstname']
+      selectFields: ['email', 'hash']
     };
 
-    encrypt('kkk', console.log);
   })();
 }
 
