ad9bb59222c73c9012a4724f0f3a27d0834b6fc5
Kanika Sharma <kanikash4@gmail.com>
2017-10-11 22:47:11 +0530

    ChatPage Additions

---
+++ public/chat.js
@@ -0,0 +1,34 @@
+window.onload = function() {
+
+    var messages = [];
+    var socket = io.connect('http://localhost:3700');
+    var field = document.getElementById("field");
+    var sendButton = document.getElementById("send");
+    var content = document.getElementById("content");
+    var name = document.getElementById("name");
+
+    socket.on('message', function (data) {
+        if(data.message) {
+            messages.push(data.message);
+            var html = '';
+            for(var i=0; i<messages.length; i++) {
+                html += messages[i] + '<br />';
+            }
+            content.innerHTML = html;
+            //If the the number of messages becomes too high, the user will need to scroll the div
+            content.scrollTop = content.scrollHeight;
+        } else {
+            console.log("There is a problem:", data);
+        }
+    });
+
+    sendButton.onclick = function() {
+        if(name.value === "") {
+            alert("Please type your name!");
+        } else {
+            var text = field.value;
+            socket.emit('send', { message: text, username: name.value });
+        }
+    };
+
+};
