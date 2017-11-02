91c3cc98afb6816a3b41d5061c3655ec8c41e2c9
Kanika Sharma <kanikash4@gmail.com>
2017-11-03 01:28:58 +0530

    adding login page details

--- client/components/login/login.js
+++ client/components/login/login.js
@@ -1,14 +1,54 @@
 import React from 'react';
+import './login.scss';
+import {Link, Redirect} from 'react-router-dom';
+import 'whatwg-fetch';
+// import {postData} from '../utils/fetch';
+// import {login as loginURL} from '../utils/urls';
+
 
 class Login extends React.Component {
     constructor() {
         super();
-        this.state = {};
+        this.proceedToLogin = this.proceedToLogin.bind(this);
+        this.onInputChange = this.onInputChange.bind(this);
+        this.state = {
+            userID: '',
+            password: '',
+            staySignedIn: false,
+            isUserLoggedIn: window.localStorage.isUserLoggedIn !== 'false'
+        };
+    }
+
+    proceedToLogin() {
+        // On success of login isUserLoggedIn state will be set as true
+        this.setState({isUserLoggedIn: true});
+        window.localStorage.isUserLoggedIn = true;
+    }
+
+    onInputChange(event) {
+        let obj = {};
+        obj[event.target.name] = event.target.name !== 'staySignedIn' ? event.target.value : event.target.checked;
+        this.setState(obj);
     }
 
     render() {
         return (
-            <div>This is the login screen</div>
+            // <div>This is the login screen</div>
+            <div classname='supercontainer'>
+            	<div className='logoContainer'>
+              </div>
+              <div className='loginContainer'>
+              	  <input name='userID' className='userID' type='text' value={this.state.userID} 
+                    onChange={this.onInputChange} placeholder='Email ID'
+                  />
+                  <input name='password' className='password' type='password' value={this.state.password}
+                      onChange={this.onInputChange} placeholder='Password'
+                  />
+
+                  <button onClick={this.proceedToLogin} value={this.state.staySignedIn}>Sign In</button>
+
+              </div>
+            </div>
         );
     }
 }
---
+++ client/util/urls.js
@@ -0,0 +1,5 @@
+const domainURL = 'http://localhost:3000';
+export const signup = domainURL + '/signup';
+export const login = domainURL + '/login';
+export const forgotPassword = domainURL + '/forgotPassword';
+export const dashboard = domainURL + '/dashboard';
