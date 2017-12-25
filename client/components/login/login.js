89122224e0f8ad39c8f7fbd29e3449deb2429b8f
Kanika Sharma <kanikash4@gmail.com>
2017-12-25 19:33:28 +0530

    Additions

--- client/components/login/login.js
+++ client/components/login/login.js
@@ -4,6 +4,7 @@ import {Link, Redirect} from 'react-router-dom';
 import 'whatwg-fetch';
 import {postData} from '../../util/fetch';
 import {login as loginURL} from '../../util/urls';
+import {forgotPassword as forgotPasswordURL} from '../../util/urls';
 
 
 class Login extends React.Component {
@@ -23,6 +24,8 @@ class Login extends React.Component {
         let {userID, password, staySignedIn} = this.state;
         postData('POST', loginURL, {userID, password, staySignedIn}, (resp) => {
             console.log('Success', resp);
+            window.location.href = "https://www.example.com";
+            console.log("set the new url");
         }, (error) => {
             console.log('Failure', error);
         });
@@ -37,28 +40,50 @@ class Login extends React.Component {
         this.setState(obj);
     }
 
+    handleSubmit(event){
+        event.preventDefault();
+        alert('it works!');
+    }
+
     render() {
         return (
             // <div>This is the login screen</div>
             <div className='supercontainer'>
-            	<div className='logoContainer'>
-              </div>
-              <div className='loginContainer'>
-              	  <input name='userID' className='userID' type='text' value={this.state.userID} 
-                    onChange={this.onInputChange} placeholder='Email ID'
-                  />
-                  <input name='password' className='password' type='password' value={this.state.password}
-                      onChange={this.onInputChange} placeholder='Password'
-                  />
-
-                  <button className="loginBtn" onClick={this.proceedToLogin} value={this.state.staySignedIn}>Sign In</button>
-                    <div className="forgetPassword">
-                        <a href ="#">Forgot Password?</a>
+                <div className="HeaderContainer">
+                    <div className='logoContainer'>
+                    Logo
                     </div>
-              </div>
+                </div>
+                <div className="mainBodyContainer">
+                    <div className='loginContainer'>
+                          <input name='userID' className='userID' type='text' value={this.state.userID} 
+                            onChange={this.onInputChange} placeholder='Email ID'
+                          />
+                          <input name='password' className='password' type='password' value={this.state.password}
+                              onChange={this.onInputChange} placeholder='Password'
+                          />
+                          <button className="loginBtn" onClick={this.proceedToLogin} value={this.state.staySignedIn}>Sign In</button>
+                            <div className="forgetPassword">
+                                <a href ="forgotPasswordURL">Forgot Password?</a>
+                            </div>
+                    </div>
+                    <br/>
+                    <div className="signUpContainer">
+                        <form onSubmit={this.handleSubmit}>
+                            <input type="text" name="firstName"/>
+                            <input type="text" name="lastName"/>
+                            <input type="text" name="phNumber"/>
+                            <input type="password" name="pass"/>
+                            <input type="text" name="email"/>
+                            <button>Register</button>
+                        </form>
+                    </div>
+                </div>
+                <div className="footerContainer">Footer</div>
             </div>
         );
     }
 }
 
 export default Login;
+
